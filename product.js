d309f56f1206625115c5608bf96be92a4e88e052
Thamizhnilavan0312 <thamizhnilavankmb@gmail.com>
2025-07-20 15:31:24 +0530

    responsiveSearchBar

--- product.js
+++ product.js
@@ -11,3 +11,19 @@ menuicon.addEventListener("click",function()
 closenav.addEventListener("click",function(){
     sidenav.style.right="-50%"
 })
+
+//searchBar
+
+  document.getElementById('searchInput').addEventListener('input', function () {
+    const searchValue = this.value.toLowerCase();
+    const products = document.querySelectorAll('.product');
+
+    products.forEach(function (product) {
+      const title = product.querySelector('h1').textContent.toLowerCase();
+      if (title.includes(searchValue)) {
+        product.style.display = 'block';
+      } else {
+        product.style.display = 'none';
+      }
+    });
+  });
